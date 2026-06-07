6d17585d1357c2677709806eb88c37db79e44662
user <user@example.com>
2026-06-08 01:54:51 +0800

    prepare server.js for Render deployment

--- server.js
+++ server.js
@@ -1,27 +1,12 @@
-// 教师电脑运行: node server.js
-// 然后在浏览器打开 http://你的IP:3000/display 投屏
-// 学生手机打开 http://你的IP:3000 做题
+// 部署到 Render.com 即可使用
+// 学生做题: https://你的域名/
+// 教师大屏: https://你的域名/display
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var os = require('os');
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var students = [];
-
-// 找本机局域网IP
-function getIP() {
-  var ifaces = os.networkInterfaces();
-  for (var key in ifaces) {
-    for (var i = 0; i < ifaces[key].length; i++) {
-      var addr = ifaces[key][i];
-      if (addr.family === 'IPv4' && !addr.internal) return addr.address;
-    }
-  }
-  return '127.0.0.1';
-}
-
-var HOST = getIP();
 
 var server = http.createServer(function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -35,7 +20,6 @@ var server = http.createServer(function(req, res) {
       try {
         var data = JSON.parse(body);
         data.time = new Date().toLocaleTimeString('zh-CN', {hour:'2-digit',minute:'2-digit',second:'2-digit'});
-        // 同名覆盖
         var idx = students.findIndex(function(s) { return s.name === data.name; });
         if (idx >= 0) students[idx] = data; else students.push(data);
         res.writeHead(200, {'Content-Type':'application/json'});
@@ -85,14 +69,5 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen(PORT, '0.0.0.0', function() {
-  console.log('');
-  console.log('========================================');
-  console.log('  Task 4 听力任务 · 本地服务器');
-  console.log('========================================');
-  console.log('');
-  console.log('  学生做题: http://' + HOST + ':' + PORT);
-  console.log('  教师大屏: http://' + HOST + ':' + PORT + '/display');
-  console.log('');
-  console.log('  按 Ctrl+C 停止');
-  console.log('========================================');
+  console.log('Task 4 Listening server running on port ' + PORT);
 });
